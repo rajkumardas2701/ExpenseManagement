978dab34761a63dc745ffc52ea062928dd09ee3d
rajkumardas2701 <rajkumardas2701@gmail.com>
2020-07-19 04:53:00 +0530

    added create action and new view for expense

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,9 +1,32 @@
 class ExpensesController < ApplicationController
 
+	def new
+		@expenses = Expense.new
+	end	
+
 	def index
 		@expenses = current_user.expenses
 	end
 
-	
-	
+	def create
+		@expenses = Expense.new(expense_params)
+		@expenses.User_id = current_user.id
+		respond_to do |format|
+			if @expenses.save
+				format.html { redirect_to expenses_path, notice: "Expense was successfully added."}
+			else
+				format.html { render :new }
+			end
+		end
+	end
+
+	private
+
+	def expense_params
+		params.require(:expense).permit(:name,
+																		:amount,
+																		:createdAt,
+																		:User_id
+																		)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
 	root to: 'users#index'
-
-get 'expenses', to: 'expenses#index'
-get 'groups', to: 'groups#index'
+resources :expenses
+resources :groups
 
 end
