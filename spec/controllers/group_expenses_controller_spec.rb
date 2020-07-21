cc4e96091a6d166031b19ed4ee3676268baa12d5
rajkumardas2701 <rajkumardas2701@gmail.com>
2020-07-21 17:29:46 +0530

    added test for GroupExpense controller

---
+++ spec/controllers/group_expenses_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe GroupExpensesController, type: :controller do
+  before { sign_in FactoryBot.create(:user) }
+  before { @expense = FactoryBot.create(:expense) }
+  before { @group = FactoryBot.create(:group) }
+
+
+  describe 'Create #create' do
+    context 'with valid params' do
+      it 'redirects to the current group page' do
+        post :create, params: { Expense_id: @expense.id, Group_id: @group.id }
+        expect(response).to redirect_to(@group)
+      end
+    end
+  end
+end
--- spec/factories/group_expense.rb
+++ spec/factories/group_expense.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :group_expense do
-    Expense_id { 1 }
-    Group_id { 1 }
+    Expense_id { 5 }
+    Group_id { 4 }
   end
 end
