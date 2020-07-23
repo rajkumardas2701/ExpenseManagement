f70b92e51a64f64ceb71efdc8a00928c418fea87
Pooja More <poojamore@Poojas-MacBook-Air.local>
2020-07-23 23:41:30 +0530

    modified DB files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_18_154312) do
+ActiveRecord::Schema.define(version: 2020_07_23_094223) do
 
   create_table "expenses", force: :cascade do |t|
     t.text "name"
@@ -38,6 +38,14 @@ ActiveRecord::Schema.define(version: 2020_07_18_154312) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
     t.index ["user_id"], name: "index_groups_on_user_id"
+  end
+
+  create_table "transactions", force: :cascade do |t|
+    t.string "name"
+    t.float "amount"
+    t.date "createdAt"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
