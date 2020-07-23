8fc8301cbd879e4300d70ffb627620f84e85971a
rajkumardas2701 <rajkumardas2701@gmail.com>
2020-07-23 16:11:55 +0530

    adding updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_18_154312) do
+ActiveRecord::Schema.define(version: 2020_07_23_101906) do
 
   create_table "expenses", force: :cascade do |t|
     t.text "name"
