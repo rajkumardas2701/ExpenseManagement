754db3bdd93060738dc418b2446be664f6b08b2b
Pooja More <poojamore@Poojas-MacBook-Air.local>
2020-07-23 23:25:34 +0530

    removed all migration files

--- db/migrate/20200718025330_add_user_ref_to_expense.rb
+++
@@ -1,5 +0,0 @@
-class AddUserRefToExpense < ActiveRecord::Migration[6.0]
-  def change
-    add_reference :expenses, :User, null: false, foreign_key: true
-  end
-end
--- db/migrate/20200718025930_add_expense_ref_to_group_expenses.rb
+++
@@ -1,5 +0,0 @@
-class AddExpenseRefToGroupExpenses < ActiveRecord::Migration[6.0]
-  def change
-    add_reference :group_expenses, :Expense, null: false, foreign_key: true
-  end
-end
