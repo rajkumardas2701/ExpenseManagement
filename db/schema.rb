d41c6fd49452755adc05d906bb49b0fe7967487f
Pooja More <poojamore@Poojas-MacBook-Air.local>
2020-07-23 20:08:55 +0530

    fixed migration errors

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_23_101906) do
+ActiveRecord::Schema.define(version: 2020_07_23_094223) do
 
   create_table "expenses", force: :cascade do |t|
     t.text "name"
