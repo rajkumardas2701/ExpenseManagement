cac375b25955195fa2924790f913e09c53f71cf4
Pooja More <poojamore@Poojas-MacBook-Air.local>
2020-07-26 06:38:13 +0530

    fixed expense add remove issue in Groups

--- app/helpers/expenses_helper.rb
+++ app/helpers/expenses_helper.rb
@@ -1,11 +1,11 @@
 module ExpensesHelper
   def add_remove(object, expense)
     if object == @ungrouped
-      button_to 'Add to this Group', group_expenses_path(Expense_id: expense.id, Group_id: @group.id),
+      button_to 'Add to this Group', group_expenses_path(expense_id: expense.id, group_id: @group.id),
                 method: :post,
                 class: 'btn btn-success btn-block'
     elsif object == @grouped
-      button_to 'Remove from this Group', group_expense_path(Expense_id: expense.id, Group_id: @group.id),
+      button_to 'Remove from this Group', group_expense_path(expense_id: expense.id, group_id: @group.id),
                 method: :delete,
                 data: { confirm: 'Are you sure?' },
                 class: 'btn btn-danger btn-block'
