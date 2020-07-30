b5c58e985ea93dad2683cf811b8d8a10dcdebb11
Pooja More <poojamore@Poojas-MacBook-Air.local>
2020-07-30 20:30:06 +0530

    Added Sanitization for User model to add profile picture

--- app/controllers/concerns/devise_whitelist.rb
+++ app/controllers/concerns/devise_whitelist.rb
@@ -6,6 +6,6 @@ module DeviseWhitelist
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[username email])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[username email userimage])
   end
 end
